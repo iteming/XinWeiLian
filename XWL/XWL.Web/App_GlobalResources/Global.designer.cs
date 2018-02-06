6404bda6ca1cafde463f1ba6ac594be6c3ffb3bd
iteming <coolon@126.com>
2018-02-06 10:56:18 +0800

    全部提交

--- XWL/XWL.Web/App_GlobalResources/Global.designer.cs
+++ XWL/XWL.Web/App_GlobalResources/Global.designer.cs
@@ -19,7 +19,7 @@ namespace Resources {
     // 类通过类似于 ResGen 或 Visual Studio 的工具自动生成的。
     // 若要添加或删除成员，请编辑 .ResX 文件，然后重新运行 ResGen
     // (以 /str 作为命令选项)，或重新生成 Visual Studio 项目。
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Web.Application.StronglyTypedResourceProxyBuilder", "14.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Global {
